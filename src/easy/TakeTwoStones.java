86f5dc56f3bd0f411b7b4bd7ba2b405f68a5a717
rstempa <stemparobin@gmail.com>
2023-06-15 13:22:02 +0200

    Finished take two stones

--- src/easy/TakeTwoStones.java
+++ src/easy/TakeTwoStones.java
@@ -6,6 +6,10 @@ public class TakeTwoStones {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        int input = in.nextInt();
 
+        if(input % 2 == 0)
+            System.out.println("Bob");
+        else System.out.println("Alice");
     }
 }
